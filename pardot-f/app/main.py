d8f92551ed087b0878daddb27e4d5172a29571a5
Bin Xuan Kong <binxuankong@gmail.com>
2021-11-29 10:06:44 +0800

    Replace all redirect with url_for

--- pardot-f/app/main.py
+++ pardot-f/app/main.py
@@ -30,36 +30,36 @@ def login():
 @app.route('/logout')
 def logout():
     session.clear()
-    return redirect('/')
+    return redirect(url_for('index'))
 
 @app.route('/callback')
 def callback():
     code = request.args.get('code', None)
     if code is None:
         print('Error getting code.')
-        return redirect('/')
+        return redirect(url_for('index'))
     session['code'] = code
-    return redirect('/update')
+    return redirect(url_for('update'))
 
 @app.route('/update')
 def update():
     code = session.get('code', None)
     if code is None:
-        return redirect('/login')
+        return redirect(url_for('login'))
     headers = get_token(code)
     if headers is None:
         print('Error getting auth token.')
-        return redirect('/')
+        return redirect(url_for('index'))
     try:
         updated = update_pardot_db(headers)
         if updated:
             print('Successfully update Pardot DB.')
         else:
             print('No new Pardot data to update.')
-        return redirect('/')
+        return redirect(url_for('index'))
     except Exception as e:
         print('Error in updating Pardot DB:', e)
-        return redirect('/')
+        return redirect(url_for('index'))
 
 @app.route('/result')
 def result():
@@ -67,7 +67,7 @@ def result():
     query = request.args.get('q', None)
     form = session.get('query', None)
     if user is None or query is None or form is None:
-        return redirect('/')
+        return redirect(url_for('index'))
     data = filter_result(form).to_dict('records')
     return generate_page('result.html', data=data, query=query, user=user)
 
@@ -77,7 +77,7 @@ def download():
     query = request.args.get('q', None)
     form = session.get('query', None)
     if user is None or query is None or form is None:
-        return redirect('/')
+        return redirect(url_for('index'))
     df = filter_result(form)
     filename = query + '.csv'
     return Response(df.to_csv(index=False),
